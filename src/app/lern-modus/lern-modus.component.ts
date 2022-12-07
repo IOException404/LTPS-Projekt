3b1843185601681b5983912a946b31bc3a7b3d07
IOException404 <ascendit87@gmx.de>
2022-12-07 01:38:37 +0100

    07.12.22

--- src/app/lern-modus/lern-modus.component.ts
+++ src/app/lern-modus/lern-modus.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LPIsimService } from '../shared/lpisim.service';
-import { mcQuestion } from '../shared/questions';
+import { mcQuestion, scQuestion } from '../shared/questions';
 
 type ViewState = 'default' | 'Lernmodus' | 'Single Choice' | 'Fill-In' | 'Alle Fragen' | 'Multiple Choice' | 'Statistik'
 
@@ -13,8 +13,9 @@ type ViewState = 'default' | 'Lernmodus' | 'Single Choice' | 'Fill-In' | 'Alle F
 
 export class LernModusComponent implements OnInit {
   viewState: ViewState = "default"; // Menü Nagivationsvariable, default ist das Home-menü des jeweiligen Modies
-  mcFragen: mcQuestion[]; // Alle Fragen
-  frage: mcQuestion; // Einzelne Frage
+  mcFragen: mcQuestion[]; // Alle Multiple-Choice-Fragen
+  scFragen: scQuestion[]; // Alle Single-Choice-Fragen
+  mcQuest: mcQuestion; // Einzelne  Frage
   currentArrayId: number = 0  // Klick prev und next Variable
   info: boolean = false; // Info und Inhalt Variable
   falsch: number; // Variable zur Auswertung der Statistik
@@ -25,7 +26,7 @@ export class LernModusComponent implements OnInit {
   ngOnInit(): void {
     this.mcFragen = this.fs.getAll(); // Service Variable für alle Fragen, fs wird vom Constructor übergeben
     this.currentArrayId = 0; // Varibale zum setzen der Array Position
-    this.frage = this.mcFragen[this.currentArrayId]; // Variable zum Auslesen der einzelnen Fragen
+    this.mcQuest = this.mcFragen[this.currentArrayId]; // Variable zum Auslesen der einzelnen Fragen
     }
 
   changeMode(eingabe: ViewState) { // Funktion zum navigieren des Menüs
@@ -43,7 +44,7 @@ export class LernModusComponent implements OnInit {
     this.truelies = true; // <-- Zurück-Button erzeugen
     if(this.currentArrayId < this.mcFragen.length-1) { // Zähler ist nicht größer als ArrayAnzahl/Position der Fragen und muss gleich gestellt werden! [0-9] = 1-10
       this.currentArrayId += 1; // Frage hochzählen
-      this.frage = this.mcFragen[this.currentArrayId]; // Die höchgezählten Fragen werden der Position des mcFragenArrays übergeben
+      this.mcQuest = this.mcFragen[this.currentArrayId]; // Die höchgezählten Fragen werden der Position des mcFragenArrays übergeben
       this.info = false; // Info Text ausblenden, Tipps gibt es nur bei Bedarf :)
     }
     if(this.currentArrayId >= this.mcFragen.length-1) { // Die currentID wird mit dem Wert der ArrayPosition 1zu1 gleich gestellt
@@ -57,7 +58,7 @@ export class LernModusComponent implements OnInit {
     // der vorhanden Array-Fragen-Liste [0-9]
     if(this.currentArrayId > 0) { // Unter Null gibt es keine Fragen!!!
       this.currentArrayId -= 1; // Frage zurück zählen
-      this.frage = this.mcFragen[this.currentArrayId]; // Id der mcFragen werden gleichgesetzt
+      this.mcQuest = this.mcFragen[this.currentArrayId]; // Id der mcFragen werden gleichgesetzt
       this.info = false; // Info Text zurück setzen
       this.richtig -= 1; // Variable rechnet richtige Antworten ab, ansonsten drohen zuviele richtige Antworten die nicht sein können!!!
     }
@@ -72,7 +73,7 @@ export class LernModusComponent implements OnInit {
 
   checkAnswers() { // Richtig- oder Falsch-Funktion der Fragen
     let fehler:boolean = false; // Am Anfang gibt es weder richtig, noch falsch!
-    for(let ele of this.frage.ans) { // Hier wird eine künstliche Liste der Antworten erstellt, szsgn. als Maske!
+    for(let ele of this.mcQuest.ans) { // Hier wird eine künstliche Liste der Antworten erstellt, szsgn. als Maske!
       if((ele.choosen && !ele.right) || (!ele.choosen && ele.right)) // Wenn die gewählte Antwort ungleich der Richtigen und die falsche Antwort ungleich der Richtigen ist, dann...
       {
         fehler = true; // Falsche Antwort!!!
